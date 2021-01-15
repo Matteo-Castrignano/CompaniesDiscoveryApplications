0a8b5b11f93be8e94feb1ecbc7bf44d49c939fb6
Matteo-Castrignano <matteo.castrignano@gmail.com>
2021-01-15 11:42:17 +0100

    lista simboli ok scarico attributi companies

---
+++ src/main/java/API/companyAttributes.java
@@ -0,0 +1,97 @@
+package API;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class companyAttributes {
+
+    private static final String key = "1b03f4651b64d5649bb16296b403b6f2";
+    //private static final String key = "b0c73ab87858bbe3a09d0a0ddcd52529";
+    private static final String urlCompany = "https://financialmodelingprep.com/api/v3/profile/";
+
+    public static void companyInformations(List<String> ListSymbols) throws IOException {
+
+        FileWriter file = new FileWriter("C:/Users/matte/Desktop/Companies.json");
+
+        for(int i=0; i<ListSymbols.size(); i++){
+
+            String json = "";
+            URL url = new URL(urlCompany + ListSymbols.get(i) + "?apikey=" + key);
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))) {
+
+                for (String line; (line = reader.readLine()) != null; ) {
+                    json += line;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            JSONArray arr = new JSONArray(json);
+
+            if(!arr.isEmpty()) {
+                String companyName = arr.getJSONObject(0).getString("companyName");
+                String symbol = arr.getJSONObject(0).getString("symbol");
+                String exchangeShortName = arr.getJSONObject(0).getString("exchangeShortName");
+                String sector = arr.getJSONObject(0).getString("sector");
+                String website = arr.getJSONObject(0).getString("website");
+                String ceo = arr.getJSONObject(0).getString("ceo");
+                String fullTimeEmployees = arr.getJSONObject(0).getString("fullTimeEmployees");
+                String phone = arr.getJSONObject(0).getString("phone");
+                String address = arr.getJSONObject(0).getString("address");
+                String city = arr.getJSONObject(0).getString("city");
+                String state = arr.getJSONObject(0).getString("state");
+                String ipoDate = arr.getJSONObject(0).getString("ipoDate");
+
+
+                JSONObject jo = new JSONObject();
+
+                jo.put("companyName", companyName);
+                jo.put("symbol", symbol);
+                jo.put("exchangeShortName", exchangeShortName);
+                jo.put("sector", sector);
+                jo.put("website", website);
+                jo.put("ceo", ceo);
+                jo.put("fullTimeEmployees", fullTimeEmployees);
+                jo.put("phone", phone);
+                jo.put("address", address);
+                jo.put("city", city);
+                jo.put("state", state);
+                jo.put("ipoDate", ipoDate);
+
+                file.write(jo.toString());
+                if (i < ListSymbols.size() - 1)
+                    file.write(",\r\n");
+
+                file.flush();
+                System.out.println(i);
+            }
+
+        }
+        file.close();
+
+    }
+
+    public static List<String> readFileSimbol() {
+
+        List<String> listSymbols = new ArrayList<>();
+
+        FileReader file;
+
+        return listSymbols;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        companyInformations(readFileSimbol());
+    }
+
+}
--- src/main/java/API/listaSimboli.java
+++ src/main/java/API/listaSimboli.java
@@ -2,7 +2,10 @@ package API;
 
 
 import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -13,10 +16,11 @@ import java.util.List;
 
 public class listaSimboli {
 
-    private static final String key = "b0c73ab87858bbe3a09d0a0ddcd52529";
+    private static final String key = "1b03f4651b64d5649bb16296b403b6f2";
+    //private static final String key = "b0c73ab87858bbe3a09d0a0ddcd52529";
     private static final String urlDowjones = "https://financialmodelingprep.com/api/v3/historical/dowjones_constituent?apikey=";
     private static final String urlNasdaq = "https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=";
-    private static final String urlCompany = "https://financialmodelingprep.com/api/v3/profile/";
+
 
     public static List<String> Dowjones_list() throws MalformedURLException {
         URL url = new URL(urlDowjones+key);
@@ -38,9 +42,6 @@ public class listaSimboli {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        //System.out.println(listSymbols);
-        //System.out.println(listSymbols.size());
 
         return listSymbols;
     }
@@ -66,72 +67,23 @@ public class listaSimboli {
             e.printStackTrace();
         }
 
-        //System.out.println(listSymbols);
-        //System.out.println(listSymbols.size());
-
         return listSymbols;
     }
 
-    public static void companyInformations() throws MalformedURLException {
+    public static void main(String[] args) throws IOException {
 
         List<String> ListSymbols = new ArrayList<>();
         ListSymbols.addAll(Dowjones_list());
         ListSymbols.addAll(Nasdaq_list());
-        List<String> attributes = new ArrayList<>();
-        String json = "";
 
-        //for(int i=0; i< ListSymbols.size(); i++)
-        for(int i=0; i< 2; i++) {
+        FileWriter file = new FileWriter("C:/Users/matte/Desktop/ListaSimboli.txt");
 
-            URL url = new URL(urlCompany + ListSymbols.get(i) + "?apikey=" + key);
+        for(int i=0; i< ListSymbols.size(); i++ ){
+            file.write(ListSymbols.get(i)+"\r\n");
+            file.flush();
+        }
+        file.close();
 
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))) {
-
-                for (String line; (line = reader.readLine()) != null; ) {
-                    json += line;
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            JSONArray arr = new JSONArray(json);
-
-            String companyName = arr.getJSONObject(0).getString("companyName");
-            String symbol = arr.getJSONObject(0).getString("symbol");
-            String exchangeShortName = arr.getJSONObject(0).getString("exchangeShortName");
-            String sector = arr.getJSONObject(0).getString("sector");
-            String website = arr.getJSONObject(0).getString("website");
-            String ceo = arr.getJSONObject(0).getString("ceo");
-            String fullTimeEmployees = arr.getJSONObject(0).getString("fullTimeEmployees");
-            String phone = arr.getJSONObject(0).getString("phone");
-            String address = arr.getJSONObject(0).getString("address");
-            String city = arr.getJSONObject(0).getString("city");
-            String state = arr.getJSONObject(0).getString("state");
-            String ipoDate = arr.getJSONObject(0).getString("ipoDate");
-
-            attributes.add(companyName);
-            attributes.add(symbol);
-            attributes.add(exchangeShortName);
-            attributes.add(sector);
-            attributes.add(website);
-            attributes.add(ceo);
-            attributes.add(fullTimeEmployees);
-            attributes.add(phone);
-            attributes.add(address);
-            attributes.add(city);
-            attributes.add(state);
-            attributes.add(ipoDate);
-
-            System.out.println(attributes);
-
-            }
-
-    }
-
-
-    public static void main(String[] args) throws MalformedURLException {
-        companyInformations();
     }
 
 }
