90dc9f3afcb96888a77f09f5c4465289a731b673
matteo <matteo.castrignano@gmail.com>
2021-01-15 18:08:05 +0100

    api per ricercare i dati delle quote non vanno una sega

--- src/main/java/API/retriveQuotes.java
+++ src/main/java/API/retriveQuotes.java
@@ -1,6 +1,28 @@
 package API;
 
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
+
+import static API.companyAttributes.readFileSimbol;
+
 public class retriveQuotes {
 
+    public void downloadQuotes() throws IOException {
+
+        List<String> listSymbol = readFileSimbol();
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Stock stock = YahooFinance.get("TSLA");
+
+       stock.print();
+
+    }
 
 }
