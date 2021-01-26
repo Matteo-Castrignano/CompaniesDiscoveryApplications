1c26a8ec66d33c745642f59b408dc43cc772be53
Matteo-Castrignano <matteo.castrignano@gmail.com>
2021-01-26 12:02:16 +0100

    complete

--- src/main/java/API/NestedDocuments.java
+++ src/main/java/API/NestedDocuments.java
@@ -1,13 +1,10 @@
 package API;
 
-import Entities.Companies;
-import com.google.gson.reflect.TypeToken;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -15,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NestedDocuments {
-    private static final Type COMP_TYPE = new TypeToken<List<Companies>>(){}.getType();
 
     public static void main(String[] args) throws IOException {
 
@@ -26,7 +22,7 @@ public class NestedDocuments {
         listSummary.add(new JSONArray(new String(Files.readAllBytes(Paths.get("C:/Users/matte/Desktop/Summary2_mc.json")), StandardCharsets.UTF_8)));
         listSummary.add(new JSONArray(new String(Files.readAllBytes(Paths.get("C:/Users/matte/Desktop/Summary3_mc.json")), StandardCharsets.UTF_8)));
 
-        FileWriter file = new FileWriter("C:/Users/matte/Desktop/Companies5.json");
+        FileWriter file = new FileWriter("C:/Users/matte/Desktop/Companies_All.json");
 
         JSONArray listCompanies = new JSONArray(textCompanies);
 
@@ -41,28 +37,28 @@ public class NestedDocuments {
             company.put("Sector", listCompanies.getJSONObject(i).getString("Sector"));
             company.put("Summary", new JSONArray());
 
-        for(int k = 0; k < listSummary.size(); k++) {
-            for (int j = 0; j < listSummary.get(k).length(); j++) {
-                if (listCompanies.getJSONObject(i).getString("Symbol").equals(listSummary.get(k).getJSONObject(j).getString("Symbol"))) {
+            for (JSONArray objects : listSummary) {
+                for (int j = 0; j < objects.length(); j++) {
+                    if (listCompanies.getJSONObject(i).getString("Symbol").equals(objects.getJSONObject(j).getString("Symbol"))) {
 
-                    JSONObject summary = new JSONObject();
-                    summary.put("Date", listSummary.get(k).getJSONObject(j).getString("Date"));
-                    summary.put("Close", listSummary.get(k).getJSONObject(j).getFloat("Close"));
-                    summary.put("Open", listSummary.get(k).getJSONObject(j).getFloat("Open"));
-                    summary.put("Volume", listSummary.get(k).getJSONObject(j).getFloat("Volume"));
-                    summary.put("Avg_Volume", listSummary.get(k).getJSONObject(j).getFloat("Avg_Volume"));
-                    summary.put("Market_cap", listSummary.get(k).getJSONObject(j).getFloat("Market_cap"));
-                    summary.put("PE_Ratio", listSummary.get(k).getJSONObject(j).getFloat("PE_Ratio"));
-                    summary.put("EPS", listSummary.get(k).getJSONObject(j).getFloat("EPS"));
-                    summary.put("Dividend", listSummary.get(k).getJSONObject(j).getString("Dividend"));
-                    summary.put("Target_prize", listSummary.get(k).getJSONObject(j).getFloat("Target_prize"));
+                        JSONObject summary = new JSONObject();
+                        summary.put("Date", objects.getJSONObject(j).getString("Date"));
+                        summary.put("Close", objects.getJSONObject(j).getFloat("Close"));
+                        summary.put("Open", objects.getJSONObject(j).getFloat("Open"));
+                        summary.put("Volume", objects.getJSONObject(j).getFloat("Volume"));
+                        summary.put("Avg_Volume", objects.getJSONObject(j).getFloat("Avg_Volume"));
+                        summary.put("Market_cap", objects.getJSONObject(j).getFloat("Market_cap"));
+                        summary.put("PE_Ratio", objects.getJSONObject(j).getFloat("PE_Ratio"));
+                        summary.put("EPS", objects.getJSONObject(j).getFloat("EPS"));
+                        summary.put("Dividend", objects.getJSONObject(j).getString("Dividend"));
+                        summary.put("Target_prize", objects.getJSONObject(j).getFloat("Target_prize"));
 
-                    company.append("Summary", summary);
+                        company.append("Summary", summary);
 
-                    break;
+                        break;
+                    }
                 }
             }
-        }
 
         file.write(company.toString(spacesToIndentEachLevel));
         file.flush();
