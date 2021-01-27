a157caeea3cb67da863143e702da730da7e5455d
Matteo-Castrignano <matteo.castrignano@gmail.com>
2021-01-27 11:50:30 +0100

    analytic are working

--- src/main/java/MongoDB/Analitics.java
+++ src/main/java/MongoDB/Analitics.java
@@ -8,23 +8,29 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.json.JsonWriterSettings;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
 import static com.mongodb.client.model.Aggregates.*;
-import static com.mongodb.client.model.Filters.lt;
-import static com.mongodb.client.model.Filters.ne;
+import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Sorts.*;
 
 
 public class Analitics {
 
     public static void main(String[] args) {
-        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27020/");
+        MongoClient mongoClient = MongoClients.create("mongodb://172.16.3.150:27020/");
 
         MongoDatabase database = mongoClient.getDatabase("CompaniesApplication");
+
+        //Analytics1(database);
+
+        //Analytics2(database, 100);
+
+        //Analytics3(database, 100000000000L);
 
     }
 
@@ -37,7 +43,7 @@ public class Analitics {
     }
 
 
-    public void Analitcs1(MongoDatabase database)
+    public static void Analytics1(MongoDatabase database)
     {
         MongoCollection<Document> collection = database.getCollection("history");
 
@@ -45,7 +51,7 @@ public class Analitics {
                         new Document("_id", new Document("symbol", "$Symbol")
                                     .append("year", new Document("$year", new Document("$dateFromString", new Document("dateString","$Date").append("format", "%Y-%m-%d"))))
                                     .append("month", new Document("$month", new Document("$dateFromString", new Document("dateString","$Date").append("format", "%Y-%m-%d"))))
-                                    .append("day", new Document("$day", new Document("$dateFromString", new Document("dateString","$Date").append("format", "%Y-%m-%d"))))
+                                    .append("day", new Document("$dayOfMonth", new Document("$dateFromString", new Document("dateString","$Date").append("format", "%Y-%m-%d"))))
                                     .append("close", "$Close")));
 
         Bson sort1 = sort(ascending("_id"));
@@ -57,7 +63,8 @@ public class Analitics {
 
         Bson sort2 = sort(ascending("_id"));
 
-        Bson addFields = new Document("$addFields", new Document("$subtract", Arrays.asList("$lastCloseMonth","$firstCloseMonth"))); //"[$lastCloseMonth,$firstCloseMonth]"
+        Bson addFields = new Document("$addFields", new Document("differenceMounth",
+                            new Document("$subtract", Arrays.asList("$lastCloseMonth","$firstCloseMonth")))); //"[$lastCloseMonth,$firstCloseMonth]"
 
         Bson sort3 = sort(orderBy(ascending("_id.year"), descending("differenceMounth")));
 
@@ -68,24 +75,24 @@ public class Analitics {
 
         Bson sort4 = sort(ascending("_id"));
 
-        List<Document> results = collection.aggregate(Arrays.asList(group1, sort1, group2, sort2, addFields, sort3, group3, sort4)).into(new ArrayList<>());
+        List<Document> results = collection.aggregate(Arrays.asList(group1, sort1, group2, sort2, addFields, sort3, group3, sort4)).allowDiskUse(true).into(new ArrayList<>());
 
         System.out.println("Nell'arco degli ultimi 5 anni, trovare per ogni azienda quale sia stato il mese dell'anno più redditizio secondo il valore di chiusura");
         results.forEach(printFormattedDocuments());
     }
 
 
-    public void Analitcs2(MongoDatabase database, float AvgVolume)
+    public static void Analytics2(MongoDatabase database, float AvgVolume)
     {
-        MongoCollection<Document> collection = database.getCollection("history");
+        MongoCollection<Document> collection = database.getCollection("companies");
 
-        Bson match1 = match(lt("AvgVolume", AvgVolume));
+        Bson match1 = match(lt("Summary.Avg_Volume", AvgVolume));
 
-        Bson unwind1 = unwind("Summary");
+        Bson unwind1 = unwind("$Summary");
 
         Bson project1 = new Document("$project",
                 new Document("_id", new Document("symbol", "$Symbol"))
-                        .append("count", new Document("$cond", Arrays.asList("$gt: [ \"$Close\" , \"$Open\" ]", 1, 0)))); //da rivedere
+                        .append("count", new Document("$cond", Arrays.asList( new Document("$gt",Arrays.asList("$Summary.Volume" , "$Summary.Avg_Volume")), 1, 0)))); //"$gt: [ \"$Summary.Volume\" , \"$Summary.Avg_Volume\"
 
         Bson group1 = new Document("$group",
                 new Document("_id", new Document("symbol","$_id.symbol"))
@@ -93,26 +100,28 @@ public class Analitics {
 
         Bson project2 = new Document("$project",
                 new Document("_id", new Document("symbol", "$_id.symbol"))
-                        .append("interesse", new Document("$cond", Arrays.asList("$gt: [ \"$tot\" , 5 ]", "yes", "no")))); //da rivedere
+                        .append("interesse", new Document("$cond", Arrays.asList( new Document("$gt", Arrays.asList("$tot" , 2)) , "yes", "no"))));
 
-        List<Document> results = collection.aggregate(Arrays.asList(match1, unwind1, project1, group1, project2)).into(new ArrayList<>());
+        Bson sort = sort(descending("interesse"));
+
+        List<Document> results = collection.aggregate(Arrays.asList(match1, unwind1, project1, group1, project2, sort)).into(new ArrayList<>());
 
         System.out.println("Per ogni titolo verificare se nell'ultima settimana l'interesse è cresiuto o diminuito, ovvero se per ogni giorno il volume è maggiore del volume medio");
         results.forEach(printFormattedDocuments());
     }
 
-    public void Analitcs3(MongoDatabase database, float cap)
+    public static void Analytics3(MongoDatabase database, long cap)
     {
-        MongoCollection<Document> collection = database.getCollection("history");
+        MongoCollection<Document> collection = database.getCollection("companies");
 
-        Bson match1 = match(lt("Market_cap", cap));
+        Bson match1 = match(lt("Summary.Market_cap", cap));
 
-        Bson unwind1 = unwind("Summary");
+        Bson unwind1 = unwind("$Summary");
 
-        Bson sort1 = sort(descending("Date"));
+        Bson sort1 = sort(descending("Summary.Date"));
 
         Bson group1 = new Document("$group",
-                new Document("_id", new Document("sector", "$Sector").append("symbol","$_id.symbol"))
+                new Document("_id", new Document("sector", "$Sector").append("symbol","$Symbol"))
                         .append("data", new Document("$last", "$Summary.Date"))
                         .append("PE", new Document("$last", "$Summary.PE_Ratio")));
 
